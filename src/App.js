81c94d12a663c940c8693b5a0096089000586a9a
Aswin <aswinramrm1061@gmail.com>
2026-07-03 14:03:17 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/about" element={<AboutSection />} />
-        <Route path="/property/:id" element={<PropertyDetails/>} />
-         <Route path="/property" element={<PropertyListing />} />
-         <Route path="/browser" element={<PropertyListing />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/property" element={<PropertyListing />} />
+        <Route path="/browser" element={<PropertyListing />} />
         <Route path="/contact" element={<ContactSection />} />
       </Routes>
     </BrowserRouter>
